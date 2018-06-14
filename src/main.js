27f0f769b1334a3946c0c11204b610e1212fd391
DanielDemi <799732469@qq.com>
2018-06-14 09:32:42 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'babel-polyfill';
+import i18n from './i18n';
+import moment from 'moment';
 
 Vue.config.productionTip = false
+
+// 引入moment用来方便设置日期
+Vue.prototype.$moment = moment;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
