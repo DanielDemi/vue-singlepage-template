6d9f10c3300b3f41c22cd679a210dc44400d3046
DanielDemi <799732469@qq.com>
2018-06-14 21:04:11 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'babel-polyfill';
+import 'babel-polyfill'; // 兼容ie es语法
 import i18n from './i18n';
-import moment from 'moment';
+// import moment from 'moment'; lodash基础工具
 
 Vue.config.productionTip = false
 
 // 引入moment用来方便设置日期
-Vue.prototype.$moment = moment;
+// Vue.prototype.$moment = moment;
 
 /* eslint-disable no-new */
 new Vue({
